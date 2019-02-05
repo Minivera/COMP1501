36db9f2d2d852d7cbd682fa672344b7194b77f2d
Guillaume St-Pierre <guillaume@manifold.co>
2019-02-05 13:00:59 -0500

    Update Unit.py

--- Assignment2/game/objects/Unit.py
+++ Assignment2/game/objects/Unit.py
@@ -1,5 +1,4 @@
-from entities.Animated import Animated
-from entities.Emote import Emote
+from entities.AnimatedWithEmote import AnimatedWithEmote, Emote
 
 
 class Unit:
@@ -21,47 +20,50 @@ class Unit:
 
     run_speed = 2
 
-    def __init__(self, unit_type, emote, orientation, left_corner, square_size, position):
-        self.left_corner = left_corner
+    def __init__(self, unit_type, emote, orientation, square_size, grid_left_corner, location_on_grid):
         self.square_size = square_size
-        self.position = position
-        self.initial_position = position
-        self.future_position = position
+        self.grid_left_corner = grid_left_corner
+        self.location = location_on_grid
+        self.position = self.__get_pos(location_on_grid)
+        self.initial_position = self.__get_pos(location_on_grid)
+        self.future_position = self.__get_pos(location_on_grid)
         self.unit_type = unit_type
         self.emote = emote
         self.orientation = orientation
-        self.entity = False
-        self.emote_entity = False
+        self.entity = None
         self.set_entity("idle")
 
     def set_entity(self, state):
-        self.entity = Animated(
+        self.entity = AnimatedWithEmote(
             self.images[self.unit_type],
             state,
             self.animations[state],
+            self.emote,
             self.scale,
-            self.__get_pos(),
+            self.position,
+            (self.square_size, self.square_size),
             self.orientation
         )
-        self.emote_entity = Emote(self.emote, 2, self.__get_pos())
 
-    def __get_pos(self):
+    def __get_pos(self, position):
         return (
-            self.left_corner[0] + self.position[0] * self.square_size - self.square_size / 2,
-            self.left_corner[1] + self.position[1] * self.square_size - self.square_size / 2,
+            self.grid_left_corner[0] + position[0] * self.square_size,
+            self.grid_left_corner[1] + position[1] * self.square_size,
         )
 
     def rotate(self, orientation):
         self.orientation = orientation
         self.entity.rotate(orientation)
 
-    def run_to(self, new_position):
-        self.future_position = new_position
+    def run_to(self, new_location):
+        self.location = new_location
+        self.future_position = self.__get_pos(new_location)
         self.entity.change_state("run")
 
-    def move_to(self, new_position):
-        self.position = new_position
-        self.future_position = new_position
+    def move_to(self, new_location):
+        self.location = new_location
+        self.position = self.__get_pos(new_location)
+        self.future_position = self.__get_pos(new_location)
 
     def reset_position(self):
         self.entity.reset_position()
@@ -71,9 +73,10 @@ class Unit:
     def change_emote(self, new_emote):
         if self.emote not in Emote.emote_positions:
             self.emote = False
+            self.entity.change_type(False)
             return
         self.emote = new_emote
-        self.emote_entity.change_type(new_emote)
+        self.entity.change_type(new_emote)
 
     def has_emote(self):
         return False if not self.emote or self.emote not in Emote.emote_positions else True
@@ -103,8 +106,6 @@ class Unit:
             self.entity.change_state("idle")
 
         self.entity.animate()
-        self.emote_entity.animate()
 
     def clean(self):
         self.entity.kill()
-        self.emote_entity.kill()
